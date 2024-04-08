6bb39f5766471e1ca225ce966a0247e706c210c1
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 20:16:32 +0500

    Day 1

---
+++ Day1/Q1/main.ts
@@ -0,0 +1,2 @@
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+// All above softwares are installed.
---
+++ Day1/Q2/main.ts
@@ -0,0 +1,4 @@
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let myName: string = "Muhammad Ali" 
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`)
