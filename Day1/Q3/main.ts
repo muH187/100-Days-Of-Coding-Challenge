99b8a80d4ad8004e0434800bffd0e15125587e9b
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 20:29:24 +0500

    Q3

--- Day1/Q3/main.ts
+++ Day1/Q3/main.ts
@@ -1,1 +1,7 @@
 // Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let newName: string = "Muhammad Ali"
+
+console.log(newName.toLowerCase())
+console.log(newName.toUpperCase())
+console.log(newName.charAt(0).toUpperCase() + newName.slice(1))
