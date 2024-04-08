62f0cd7b52b2bfae8863664e9d771528841ef25e
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 22:27:00 +0500

    Q11

---
+++ Day4/Q11/main.ts
@@ -0,0 +1,10 @@
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friendsName = ["Muhammad Mirza", "Abdul Wali", "Yasir", "Bilal", "Zohaib", "Hassan"]
+
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
+console.log(friendsName[3]);
+console.log(friendsName[4]);
+console.log(friendsName[5]);
