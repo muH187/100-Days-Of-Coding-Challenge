6d442d32a3bb025bba86ffa44c495b7b9cd4a162
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 22:21:42 +0500

    Q10

---
+++ Day4/Q10/main.ts
@@ -0,0 +1,7 @@
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+
+console.log(4+4);
+console.log(12-4);
+console.log(4*2);
+console.log(32/4);
+// Name: Muhammad Ali, Date: 8 April 2024, This above code is doing simple calculation and getting 8 number of each line.
