b0e74080b7ebe0564185cc02aee41db624e15faa
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 22:06:08 +0500

    Q9

---
+++ Day3/Q9/main.ts
@@ -0,0 +1,5 @@
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+let favorite_number: number = 8
+console.log(`The number ${favorite_number} is my favorite number.`);
+
