c85ca8964b375b6947d997fdd48d5a13e2e107c0
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 21:39:51 +0500

    Day2

--- Day2/Q1/main.ts
+++ Day2/Q1/main.ts
@@ -1,7 +1,5 @@
 // Question 1: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 // "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
 
-let authorName: string = "Warren Buffet once said,"
-let authorQuote: string = "If you don't find a way to make money while you sleep, you will work until you die."
+console.log("Warren Buffet once said, If you don't find a way to make money while you sleep, you will work until you die.");
 
-console.log(`${authorName} ${authorQuote}`)
---
+++ Day2/Q2/main.ts
@@ -0,0 +1,6 @@
+// Question 2: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "Warren Buffet once said,"
+let message: string = "If you don't find a way to make money while you sleep, you will work until you die."
+
+console.log(`${famous_person} ${message}`)
