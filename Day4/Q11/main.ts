90aad1c882afc42a2151cbf0c05d82d522616b71
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-08 22:38:05 +0500

    Q12

--- Day4/Q11/main.ts
+++ Day4/Q11/main.ts
@@ -1,6 +1,6 @@
 // Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
 
-let friendsName = ["Muhammad Mirza", "Abdul Wali", "Yasir", "Bilal", "Zohaib", "Hassan"]
+let friendsName:string[] = ["Muhammad Mirza", "Abdul Wali", "Yasir", "Bilal", "Zohaib", "Hassan"]
 
 console.log(friendsName[0]);
 console.log(friendsName[1]);
--- Day4/Q12/main.ts
+++ Day4/Q12/main.ts
@@ -1,11 +1,8 @@
 // Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
 
-let friendsName = ["Muhammad Mirza", "Abdul Wali", "Yasir", "Bilal", "Zohaib", "Hassan"]
-let message: string = "Hello"
+let friendsName: string[] = ["Muhammad Mirza", "Abdul Wali", "Yasir", "Bilal", "Zohaib", "Hassan"]
 
-console.log(message, friendsName[0]);
-console.log(message, friendsName[1]);
-console.log(message, friendsName[2]);
-console.log(message, friendsName[3]);
-console.log(message, friendsName[4]);
-console.log(message, friendsName[5]);
+for( let name of friendsName) {
+    console.log("Hello", name);
+    
+}
