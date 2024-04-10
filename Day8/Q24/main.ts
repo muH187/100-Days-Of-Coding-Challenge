36919ac5231249ce428702a03bbc48fa7d5478e5
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-10 21:46:14 +0500

    Q24

---
+++ Day8/Q24/main.ts
@@ -0,0 +1,11 @@
+// Question 24: More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
+
+let fruit: string[] = ["Mango", "Apple", "Kivi", "Banana"];
+
+console.log("Is Kivi in fruit, I predict True");
+console.log(fruit.includes("Kivi"));
+
+console.log("Is Orange in fruit, I predict False");
+console.log(!fruit.includes("Orange"));
+
+
