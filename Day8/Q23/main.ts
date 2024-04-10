4a72298c60da29326baf72f920af97d4874a50dc
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-10 20:28:53 +0500

    Q23

---
+++ Day8/Q23/main.ts
@@ -0,0 +1,17 @@
+// Question 23: Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+
+let book: string = "Mathematics"
+
+console.log("Is book = Mathematics, I predict True.");
+console.log(book == "Mathematics");
+
+console.log("Is book = Science, I predict False.");
+console.log(book == "Science");
+
+
+
+
+
+
+
+
