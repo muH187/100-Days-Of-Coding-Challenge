13f2b6d61dc0696481e967109c2e59bb1cbe60d0
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-10 10:20:07 +0500

    Q13

---
+++ Day5/Q13/main.ts
@@ -0,0 +1,8 @@
+// Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let favorite_car: string[] = ["Tucson", "Fortuner", "Sonata", "Civic"]
+
+favorite_car.forEach(car => {
+    console.log(`I would like to own a ${car} car.`);
+    
+})
