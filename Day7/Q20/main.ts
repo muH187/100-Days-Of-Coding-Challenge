12311fdd2e4fa0de648af8313153bf8d3d88f41d
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-10 20:07:35 +0500

    Q20

---
+++ Day7/Q20/main.ts
@@ -0,0 +1,5 @@
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let languages: string[] = ["English", "Chinese", "Arabic", "German", "Spanish"]
+
+console.log(`Languages I would like to learn ${languages}`);
