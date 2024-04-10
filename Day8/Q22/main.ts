12fcd631514d5222b8d55c4ece87d541ed672f47
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-10 20:19:47 +0500

    Q22

---
+++ Day8/Q22/main.ts
@@ -0,0 +1,5 @@
+// Question 22: Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+
+let dinner_invitation: string [] = ["Muhammad Mirza", "Yasir", "Bilal"]
+
+console.log(dinner_invitation[3]);
