a82761e93e243bc805869f88292206c36f7f8736
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-11 06:15:48 +0500

    Day9

---
+++ Day9/main.ts
@@ -0,0 +1,35 @@
+// Question 25:
+let alien_color = "green"
+if(alien_color == "green") {
+    console.log("Alien shot down.");  
+}
+if (alien_color == "red") {
+    
+}
+
+// Question 26:
+if ( alien_color == "green") {
+    console.log("The color of alien is Green");
+} else {
+    console.log("The color of alien is not Green.");
+}
+
+// Question 27:
+let alien_colors = "green"
+if ( alien_colors == "green") {
+    console.log("The color of alien is green")
+} else {
+    console.log("Your alien shot down");
+}
+alien_colors = "yellow"
+if ( alien_colors == "yellow") {
+    console.log("The color of alien is yellow")
+} else {
+    console.log("Your alien shot down");
+}
+alien_colors = "red"
+if ( alien_colors == "blue") {
+    console.log("The color of alien is red")
+} else {
+    console.log("Your alien shot down");
+}
