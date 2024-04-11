d5edf3fd88474c1a6177721ac2807b8d14c87c1b
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-11 06:25:51 +0500

    Question 28

---
+++ Day10/main.ts
@@ -0,0 +1,15 @@
+// Question 28:
+let age: number = 19
+if( age <= 3) {
+    console.log("You are baby");
+} else if ( age <= 12) {
+    console.log("You are kid");
+} else if ( age <= 19) {
+    console.log("You are teenager");
+} else if ( age <= 29) {
+    console.log("You are in your 20s");
+} else if ( age <= 59) {
+    console.log("You are adult");
+} else {
+    console.log("You are Senior")
+}
