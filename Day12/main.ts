8a8a8e83444141d0cd4f5cbcadeb520954af72d5
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 13:23:06 +0500

    Question 35

--- Day12/main.ts
+++ Day12/main.ts
@@ -4,3 +4,11 @@ let pizzas:string [] = ["Chicken Tikka Pizza", "Malai Botti Pizza", "Cheese Pizz
 pizzas.forEach(pizza => {
     console.log(`I like ${pizza}`);
 })
+
+// Question 35:
+let animals: string[] = ["Eagle", "Crow", "Parrot"]
+
+animals.forEach(animal => {
+    console.log(`${animal} can fly`);
+})
+console.log("All the above birds can fly");
