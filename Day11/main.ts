2de78e79945b422f9c5bdc414cd2d75a645ab855
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 13:01:27 +0500

    Question 32

--- Day11/main.ts
+++ Day11/main.ts
@@ -9,3 +9,15 @@ if(users.length === 0) {
         console.log(`Hi! How are you ${user}?`)
     })
 }
+
+// Question 32:
+let current_username: string[] = ["Ali", "Tanzeel", "Hasan", "Bilal", "Mirza"]
+let new_username: string[] = ["Umer", "Yasir", "Zohaib", "Basit", "Wali", "Bilal"]
+
+new_username.forEach(newUser => {
+    if(current_username.some(currentUser => currentUser.toLowerCase() === newUser.toLowerCase())){
+        console.log(`${newUser} username is not available.`);
+    } else {
+        console.log(`${newUser} is available.`)
+    }
+})
