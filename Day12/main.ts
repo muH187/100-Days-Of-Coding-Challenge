3ca7a53452e45a531d27d05c1eb3d1a606af0448
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 16:03:25 +0500

    Question 36

--- Day12/main.ts
+++ Day12/main.ts
@@ -12,3 +12,9 @@ animals.forEach(animal => {
     console.log(`${animal} can fly`);
 })
 console.log("All the above birds can fly");
+
+// Question 36:
+function make_Tshirts(size:string, message:string) {
+    console.log(`Making a ${size} t-shirt with the message of ${message}`)
+}
+make_Tshirts("large", "Comfortable with being uncomfortable.")
