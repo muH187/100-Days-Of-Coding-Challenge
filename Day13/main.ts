0e38cac29fd913a7d20a02ccef6b9178ed03a965
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 16:07:34 +0500

    Question 37

---
+++ Day13/main.ts
@@ -0,0 +1,6 @@
+// Question 37:
+function make_shirt(size:string = "Large", message:string = "Great things take times") {
+    console.log(`Make a ${size} t-shirt with the message of ${message}.`);
+}
+make_shirt()
+make_shirt("small", "Dive into TypeScript")
