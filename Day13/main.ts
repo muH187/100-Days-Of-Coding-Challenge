0b9deb8a71f2a7fcc5256342bf276641e57f0e71
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 16:12:59 +0500

    Question 38

--- Day13/main.ts
+++ Day13/main.ts
@@ -4,3 +4,13 @@ function make_shirt(size:string = "Large", message:string = "Great things take t
 }
 make_shirt()
 make_shirt("small", "Dive into TypeScript")
+
+// Question 38:
+function describe_city(city:string, country:string) {
+    console.log(`${city} city is in ${country}`)
+}
+
+describe_city("Karachi", "Pakistan")
+describe_city("Moscow", "Russia")
+describe_city("Beijing", "China")
+describe_city("Tokyo", "Japan")
