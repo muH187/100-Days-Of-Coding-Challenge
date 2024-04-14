7fca7e1bbea7d8c77bad8da3c9c7e2857de17750
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-14 13:07:34 +0500

    Question 33

--- Day11/main.ts
+++ Day11/main.ts
@@ -21,3 +21,18 @@ new_username.forEach(newUser => {
         console.log(`${newUser} is available.`)
     }
 })
+
+// Question 33:
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+numbers.forEach(number => {
+    let suffix = "th"
+    if(number === 1) {
+        suffix = "st"
+    } else if(number === 2) {
+        suffix = "nd"
+    } else if(number === 3) {
+        suffix = "rd"
+    }
+    console.log(`${number}${suffix}`);
+    
+})
