bd3b3ba27a3afa5579612a72e56f597ebd2a7eb9
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 21:36:20 +0500

    Question 69

--- Day23/main.ts
+++ Day23/main.ts
@@ -9,3 +9,11 @@ function multiplyDecimal(number1: number, number2: number): number {
     return Math.round(number1 * number2)
 }
 console.log(multiplyDecimal(23.6235, 10.2352));
+
+// Question 69:
+function dividentAndRemainder (divident: number, divisor: number): {quotient: number, remainder: number} {
+    let quotient = Math.floor(divident / divisor)
+    let remainder = divident % divisor
+    return { quotient, remainder }
+}
+console.log(dividentAndRemainder(10, 3));
