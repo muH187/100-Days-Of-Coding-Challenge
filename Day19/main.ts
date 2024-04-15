6dc0af2d84f8c481fc3e93eb046b4e46797469c2
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 11:48:04 +0500

    Question 57

--- Day19/main.ts
+++ Day19/main.ts
@@ -7,3 +7,9 @@ console.log(doubleNumbers);
 let mixedArray = [1, 3, "Apple", false, "Phone", 8, "Bottle", true, 5, "TypeScript"]
 let stringArray = mixedArray.filter(item => typeof item === "string")
 console.log(stringArray);
+
+// Question 57:
+let marks = [80, 100, 40, 63, 23, 65]
+let avgMarks = marks.reduce((total, marks) => total + marks, 0) / marks.length
+console.log(avgMarks);
+ 
