5381c7295e7ba088baa83fa50ba9c1ba956f3d26
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 21:27:19 +0500

    Question 68

--- Day23/main.ts
+++ Day23/main.ts
@@ -3,3 +3,9 @@ function addNumberAndString (number1: number, numberString: string) {
     return number1 + Number(numberString)
 }
 console.log(addNumberAndString(20, "30"));
+
+// Question 68:
+function multiplyDecimal(number1: number, number2: number): number {
+    return Math.round(number1 * number2)
+}
+console.log(multiplyDecimal(23.6235, 10.2352));
