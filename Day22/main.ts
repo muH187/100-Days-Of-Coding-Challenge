59aa8825ce2d34edaed719857270c0b8bbd7aa2c
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 21:15:51 +0500

    Question 65

--- Day22/main.ts
+++ Day22/main.ts
@@ -3,3 +3,9 @@ function combineStrNum(string:string, number:number) {
     return string + number
 }
 console.log(combineStrNum("Age: ", 20));
+
+// Question 65:
+function remainder(num1: number, num2: number) {
+    return num1 % num2
+}
+console.log(remainder(10, 3));
