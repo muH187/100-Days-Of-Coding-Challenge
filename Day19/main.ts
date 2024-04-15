fb0eeab1cfe2209bbf0e5d23dc1274818e1cfdad
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 11:43:36 +0500

    Question 56

--- Day19/main.ts
+++ Day19/main.ts
@@ -2,3 +2,8 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7]
 let doubleNumbers = numbers.map(number => number * 2)
 console.log(doubleNumbers);
+
+// Question 56:
+let mixedArray = [1, 3, "Apple", false, "Phone", 8, "Bottle", true, 5, "TypeScript"]
+let stringArray = mixedArray.filter(item => typeof item === "string")
+console.log(stringArray);
