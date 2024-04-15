cc17a4a4a61087ee1453da985448e27675365835
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 10:19:41 +0500

    Question 52

---
+++ Day18/main.ts
@@ -0,0 +1,10 @@
+// Question 52:
+let phone = {
+    brand: "Apple",
+    model: "iPhone 15 Pro Max",
+    screen: "6.8 Inches",
+    storage: "1TB Storage",
+    battery: "2 Day long battery life",
+    describe: function () {console.log(`This phone is an ${this.brand} ${this.model}, big screen size of ${this.screen}, has ${this.storage}, and ${this.battery}`)}
+}
+phone.describe()
