bcac0d0a56af24b213613bb58378aedc3dc1f7d2
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 14:20:29 +0500

    Question 62

--- Day21/main.ts
+++ Day21/main.ts
@@ -6,3 +6,23 @@ enum VehicleType {
     Plane
 }
 console.log(VehicleType.Plane);
+
+// Question 62:
+interface Student {
+    name: string
+    age: number
+    courses: string[]
+}
+
+let student1: Student = {
+    name: "Ali",
+    age: 19,
+    courses: ["JavaScript", "TypeScript", "Python"]
+}
+let student2: Student = {
+    name: "Tanzeel",
+    age: 20,
+    courses: ["Computer", "Maths", "Business"]
+}
+console.log(student1);
+console.log(student2);
