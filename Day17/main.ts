e8671f8f7c3a69cd13f8a18ecb71304e1e1150ab
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-15 10:03:11 +0500

    Question 50

---
+++ Day17/main.ts
@@ -0,0 +1,15 @@
+// Question 49:
+function myHobbies(...hobbies:string[]) {
+    hobbies.forEach(hobby => {
+        console.log(`I enjoy ${hobby}`);
+    })
+}
+myHobbies("Coding", "Reading", "Writing")
+
+// Question 50:
+let myIdealDay = `My ideal day would look like:
+1. I woke up at Fajr.
+2. I execise.
+3. I learnt many new concepts of programming.
+4. I post on linkedin.`
+console.log(myIdealDay);
