e562c7da617a0321db442b93d7da5b07b74fde15
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-16 23:11:01 +0500

    Question 74

--- Day25/main.ts
+++ Day25/main.ts
@@ -7,3 +7,15 @@ function updateVarible() {
     console.log("Final Value is: ", number);
 }
 updateVarible()
+
+// Question 74:
+function swapVariable() {
+    let a = 30, b = 50
+    console.log("Before swapping:", "a = ", a, ", b = ", b);
+    
+    let temp = a
+    a = b
+    b = temp
+    console.log("After swapping:", "a = ", a, "b = ", b)
+}
+swapVariable()
