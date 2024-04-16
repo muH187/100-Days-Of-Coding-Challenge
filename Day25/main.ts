51fc92c82e0d8e63a58e23bb4df43b98bbd54df1
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-16 23:17:57 +0500

    Question 75

--- Day25/main.ts
+++ Day25/main.ts
@@ -19,3 +19,22 @@ function swapVariable() {
     console.log("After swapping:", "a = ", a, "b = ", b)
 }
 swapVariable()
+
+// Question 75:
+function useCompound() {
+    let x = 4
+    console.log("Initial Value of x: ", x);
+    
+    x += 6
+    console.log("Adding 6 to x: ", x);
+    
+    x -= 3
+    console.log("Subtracting 3 to x: ", x);
+
+    x *= 5
+    console.log("Multiply by 5 to x: ", x);
+
+    x /= 2
+    console.log("Divide by 2 to x: ", x);
+}
+useCompound()
