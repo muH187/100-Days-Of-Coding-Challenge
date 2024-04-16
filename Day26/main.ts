88a0fbeac753804c6f5d8e98ab0bc2f21dfa0e02
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-16 23:22:24 +0500

    Question 77

---
+++ Day26/main.ts
@@ -0,0 +1,17 @@
+// Question 76:
+function add(num1:number, num2:number): number {
+    return num1 + num2
+}
+console.log(add(20, 20));
+console.log(add(18, 32));
+console.log(add(10, 50));
+
+// Question 77:
+function greet(name: string = "Anonymous") {
+    console.log("Hi! How are you", name);
+}
+greet()
+greet("Ali")
+
+
+// Question 78:
