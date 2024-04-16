34c52ec3963f9445576bc0c1c3821e23084e49f8
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-16 22:31:25 +0500

    Question 71

--- Day25/main.ts
+++ Day25/main.ts
@@ -5,3 +5,15 @@ function countDown() {
     }
 }
 countDown()
+
+// Question 71:
+let age = 19
+age = 20
+console.log(age);
+
+const name = "Ali"
+try {
+    name = "Alee"
+} catch (error) {
+    console.log("Error: Can't reassign a 'const'-declared variable.");
+}
