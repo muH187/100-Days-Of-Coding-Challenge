f0de39f0b7045dc237fd2760623265c474e80c7d
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 11:58:30 +0500

    Question 87

--- Day29/main.ts
+++ Day29/main.ts
@@ -9,3 +9,9 @@ function findText(string: string): boolean {
     return string.includes("JavaScript")
 }
 console.log(findText("I love TypeScript"));
+
+// Question 87:
+function extractingString(str1: string): string {
+    return str1.substring(0, 10)
+}
+console.log(extractingString("I love programming."));
