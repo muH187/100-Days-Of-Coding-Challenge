5b6c2a86a264247b9c89da34c42b89f7e7f49b1a
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 11:40:33 +0500

    Question 81

--- Day27/main.ts
+++ Day27/main.ts
@@ -10,3 +10,11 @@ console.log(car);
 (car as any).color = "Black"
 car.year = 2025
 console.log(car);
+
+// Question 81:
+function objProperty(obj: object) {
+    for(let property in obj) {
+        console.log(`${property}: ${obj[property]}`);
+    }
+}
+objProperty({make: "Hyundai", model: "Sonata", year: 2023, color: "White"})
