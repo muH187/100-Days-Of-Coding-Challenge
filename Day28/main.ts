b749bf6af7819d7eb3051bcafb31ba4a7b92f8c3
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 11:45:17 +0500

    Question 83

--- Day28/main.ts
+++ Day28/main.ts
@@ -3,3 +3,10 @@ function checkLenght(str: string) {
     return str.length
 }
 console.log(checkLenght("Hello World"));
+
+// Question 83:
+function convert(string: string) {
+    console.log(string.toUpperCase());
+    console.log(string.toLowerCase());
+}
+convert("Hey, I'm Muhammad Ali");
