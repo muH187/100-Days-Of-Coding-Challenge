6fd54cb158a11d912489af0223bd9a0a393293bf
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 11:55:31 +0500

    Question 86

---
+++ Day29/main.ts
@@ -0,0 +1,11 @@
+// Question 85:
+function findPosition(str: string): number {
+    return str.indexOf("code")
+}
+console.log(findPosition("I'm learning to code in TypeScript"));
+
+// Question 86:
+function findText(string: string): boolean {
+    return string.includes("JavaScript")
+}
+console.log(findText("I love TypeScript"));
