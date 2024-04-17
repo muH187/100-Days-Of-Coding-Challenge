7670a449aac65b822e573d5461f33b86df4476ee
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 15:57:51 +0500

    Question 96

--- Day32/main.ts
+++ Day32/main.ts
@@ -9,3 +9,10 @@ function greaterThanTen(number: number[]): number[] {
 }
 let counting: number[] = [2, 3, 4, 13, 66, 32, 2, 9, 10, 43, 13, 54, 7]
 console.log(greaterThanTen(counting));
+
+// Question 96:
+function sumOfAll(array: number[]): number {
+    return array.reduce((accumulator, current) => accumulator + current, 0)
+}
+const array: number[] = [5, 20, 50, 3, 2, 10, 8, 1, 1]
+console.log(sumOfAll(array));
