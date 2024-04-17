5c05bc7bd53e5041ba6af7f76d4146f83acf7afa
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 15:40:04 +0500

    Question 93

--- Day31/main.ts
+++ Day31/main.ts
@@ -11,3 +11,12 @@ function removeLastElement<T>(arr: T[]): T | undefined {
 let fruits = ["Apple", "Cherry", "Grapes"]
 console.log(removeLastElement(fruits));
 console.log(fruits)
+
+// Question 93:
+function replaceBananaWithMango(str: string[]): void {
+    const index = str.indexOf("Banana")
+    if( index !== -1) str[index] = 'Mango'
+}
+const str: string[] = ["Strawberry", "Grapes", "Banana"]
+replaceBananaWithMango(str)
+console.log(str);
