12a583ebb3c92c5ad6f1642e68402fdf91acbc02
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 15:52:31 +0500

    Question 95

--- Day32/main.ts
+++ Day32/main.ts
@@ -2,3 +2,10 @@
 const words: string[] = ["TypeScript", "Python", "C++", "Java", "Programming", "Software"]
 const lengths: number[] = words.map(word => word.length)
 console.log(lengths);
+
+// Question 95:
+function greaterThanTen(number: number[]): number[] {
+    return number.filter(numbers => numbers > 10)
+}
+let counting: number[] = [2, 3, 4, 13, 66, 32, 2, 9, 10, 43, 13, 54, 7]
+console.log(greaterThanTen(counting));
