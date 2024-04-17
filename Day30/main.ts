4a2f46087ffcb613a98104c0911a385803eb07ef
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 12:04:33 +0500

    Question 89

--- Day30/main.ts
+++ Day30/main.ts
@@ -5,3 +5,8 @@ function roundNumber(number: number): number {
 console.log(roundNumber(23.35943));
 console.log(roundNumber(23.889823));
 
+// Question 89:
+function convertStringToNumber(str: string): number {
+    return parseInt(str)
+}
+console.log(convertStringToNumber("123"));
