3c950c54e4dd4063b40a6cce199d17c43e79d534
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 11:35:42 +0500

    Question 79 and 80

---
+++ Day27/main.ts
@@ -0,0 +1,12 @@
+// Question 79:
+let car = {
+    make: "Honda",
+    model: "Civic RS Turbo",
+    year: 2024
+}
+console.log(car);
+
+// Question 80:
+(car as any).color = "Black"
+car.year = 2025
+console.log(car);
