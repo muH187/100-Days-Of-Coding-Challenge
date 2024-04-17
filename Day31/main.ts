479880f472b19abc064118391faf695cbf2dc49c
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 15:23:43 +0500

    Question 92

--- Day31/main.ts
+++ Day31/main.ts
@@ -3,3 +3,11 @@ let favoriteFruits: string[] = ["Mango", "Banana", "Watermelon"]
 console.log(favoriteFruits)
 favoriteFruits.push("Kivi")
 console.log(favoriteFruits);
+
+// Question 92:
+function removeLastElement<T>(arr: T[]): T | undefined {
+    return arr.pop()
+}
+let fruits = ["Apple", "Cherry", "Grapes"]
+console.log(removeLastElement(fruits));
+console.log(fruits)
