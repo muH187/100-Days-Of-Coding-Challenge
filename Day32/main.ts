d73fda757d6be2e69f64b4851d9c85edc1f77925
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-17 15:47:58 +0500

    Question 94

---
+++ Day32/main.ts
@@ -0,0 +1,4 @@
+// Question 94:
+const words: string[] = ["TypeScript", "Python", "C++", "Java", "Programming", "Software"]
+const lengths: number[] = words.map(word => word.length)
+console.log(lengths);
