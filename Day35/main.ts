ee6d6b3d22750bbb408139af146de6bead306447
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-18 18:36:02 +0500

    Question 104

--- Day35/main.ts
+++ Day35/main.ts
@@ -3,3 +3,11 @@ function randomBoolean (): boolean {
     return Math.random() > 0.5
 }
 console.log(randomBoolean());
+
+// Question 104:
+function getRandomHexColor(): string {
+    const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+    return color;
+}
+
+console.log(getRandomHexColor());
