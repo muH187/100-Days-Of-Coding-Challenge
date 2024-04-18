1dd719d3ac31e4137c044047ac43dcb4349ed6cd
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-18 18:29:05 +0500

    Question 101

--- Day34/main.ts
+++ Day34/main.ts
@@ -3,3 +3,7 @@ function findSquare(square: number): number {
     return Math.sqrt(square)
 }
 console.log(findSquare(144));
+
+// Question 101:
+let randomNum = Math.floor(Math.random() * 10) + 1
+console.log(randomNum);
