1e455bc3e885a52e3b15e23b848fb45d1bb2e8f0
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-18 18:31:01 +0500

    Question 102

--- Day34/main.ts
+++ Day34/main.ts
@@ -7,3 +7,7 @@ console.log(findSquare(144));
 // Question 101:
 let randomNum = Math.floor(Math.random() * 10) + 1
 console.log(randomNum);
+
+// Question 102:
+const differece: number = Math.abs(-5 - 5)
+console.log(differece);
