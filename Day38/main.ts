6707aaa40084d166313ce2f1525034f795017d12
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-19 12:23:02 +0500

    Question 114

--- Day38/main.ts
+++ Day38/main.ts
@@ -12,3 +12,15 @@ function checkMap(): string {
     return map.Canada
 }
 console.log(checkMap());
+
+// Question 114:
+const students = new Map<number, string>()
+students.set(1, "Muhammad Ali")
+students.set(2, "Muhammad Mirza")
+students.set(3, "Hassan")
+students.set(4, "Tanzeel")
+students.set(5, "Tamim")
+
+students.forEach((name, id) => {
+    console.log(`Student ID: ${id}, Name: ${name}`);
+})
