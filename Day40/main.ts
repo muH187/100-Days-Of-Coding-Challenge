e2555b937b74b3815c3ec1461e3e9c85de1d07b0
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-19 15:50:19 +0500

    Question 118 and 119

---
+++ Day40/main.ts
@@ -0,0 +1,12 @@
+// Question 118:
+for(let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+// Question 119:
+let count: number = 0
+while(count < 5) {
+    console.log("Hello World");
+    count++
+}
+// Question 120:
