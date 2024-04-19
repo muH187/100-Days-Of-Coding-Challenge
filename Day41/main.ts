892576c553cf49165dabc4f1f232ad1760c495d9
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-19 16:02:45 +0500

    Question 123

--- Day41/main.ts
+++ Day41/main.ts
@@ -15,3 +15,18 @@ while(count > 0) {
     console.log(count);
     count--
 }
+
+// Question 123:
+function logUntilVowel(str: string): void {
+    const vowels = "aeiouAEIOU";
+    for (const char of str) {
+      if (vowels.includes(char)) {
+        console.log(`First vowel found: ${char}`);
+        break;
+      }
+      console.log(char);
+    }
+  }
+  
+  logUntilVowel("syzygy");
+  
