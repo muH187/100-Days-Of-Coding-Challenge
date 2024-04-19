12a44677b34d7466368220d0b56322ce16a94cc3
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-19 15:35:41 +0500

    Question 115

---
+++ Day39/main.ts
@@ -0,0 +1,28 @@
+// Questoin 115:
+let days: number = 4
+switch(days) {
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday");
+        break
+    case 3:
+        console.log("Wednessday");
+        break
+    case 4:
+        console.log("Thursday");
+        break
+    case 5:
+        console.log("Friday");
+        break
+    case 6:
+        console.log("Saturday");
+        break
+    case 7:
+        console.log("Sunday");
+        break
+    default:
+        console.log("There are only 7 days in a week!");
+        break
+}
