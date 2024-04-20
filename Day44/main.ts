298cfa54667a0a8716e9241c326ce53111317906
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-20 12:24:45 +0500

    Question 132

--- Day44/main.ts
+++ Day44/main.ts
@@ -1,13 +1,13 @@
 // Question 130:
-import { add } from "./mathFunction"
+import { add } from "./Q130/mathFunction"
 console.log(add(10, 40))
 
 // Question 131:
-import { Person } from "./person"
+import { Person } from "./Q131/person"
 const boy = new Person("Ali")
 boy.greet()
 
 // Question 132:
-import { Calculator } from "./Calculator"
+import { Calculator } from "./Q132/Calculator"
 
-import { utilOne, utilTwo } from "./utils"
+import { utilOne, utilTwo } from "./Q132/utils"
