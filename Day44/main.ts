1b1997f11106268c10bd347e997d1ece66e36ba5
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-20 12:22:23 +0500

    Question 132

--- Day44/main.ts
+++ Day44/main.ts
@@ -6,3 +6,8 @@ console.log(add(10, 40))
 import { Person } from "./person"
 const boy = new Person("Ali")
 boy.greet()
+
+// Question 132:
+import { Calculator } from "./Calculator"
+
+import { utilOne, utilTwo } from "./utils"
---
+++ Day44/utils.ts
@@ -0,0 +1,6 @@
+export const utilOne = () => {
+    // this is utility one.
+}
+export const utilTwo = () => {
+    // this is utility two.
+}
