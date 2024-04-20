b6b458f0058cfecf2d76aec279e037410b2ba026
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-20 11:34:33 +0500

    Question 124

---
+++ Day42/main.ts
@@ -0,0 +1,14 @@
+// Question 124:
+const obj = {
+    name: "Ali",
+    age: 19,
+    getDetail: function () {
+        console.log(this.name);
+        console.log(this.age);
+        
+    }
+}
+obj.getDetail()
+
+// Question 125:
+// Question 126:
