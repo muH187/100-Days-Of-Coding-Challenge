0c8edd5d493be04a98a37c0c1fe2ecc06050667f
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-21 16:25:50 +0500

    Question 143

--- Day48/main.ts
+++ Day48/main.ts
@@ -6,4 +6,18 @@ let helloPromise = new Promise<string>((resolve) => {
 })
 helloPromise.then((message) => console.log(message))
 // Question 143:
+const conditionalPromise = new Promise<string>((resolve, reject) => {
+  const success = Math.random() > 0.5;
+  if (success) {
+    resolve("Success!");
+  } else {
+    reject(new Error("Failure"));
+  }
+});
+
+conditionalPromise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
+
+
 // Question 144:
