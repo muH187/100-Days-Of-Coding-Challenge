bbf79095b73e1a2f7c8ee35e74721c69c11a592d
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-21 10:57:16 +0500

    Question 137

--- Day46/main.ts
+++ Day46/main.ts
@@ -1,4 +1,11 @@
-// Question 136
+// Question 136:
 for ( let i = 1; i <= 5; i++ ) {
     console.log(`Iteration: ${i}, i value, i`)
+}
+
+// Question 137:
+try {
+    throw new Error("Something went wrong.")
+} catch (error) {
+    console.log(error.message)
 }
