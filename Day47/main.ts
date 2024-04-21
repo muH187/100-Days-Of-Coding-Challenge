8560578b659c2dcea57c7de1d28838fcb7198b96
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-21 11:05:10 +0500

    Question 139

---
+++ Day47/main.ts
@@ -0,0 +1,12 @@
+// Question 139:
+
+// const is reserve variable:
+const country = "Pakistan"
+// The word if and return is also a reserve keywords in JS or TS.
+let number:number = 5
+if (number > 3) {
+    console.log(true)
+}
+
+// Question 140:
+// Question 141:
