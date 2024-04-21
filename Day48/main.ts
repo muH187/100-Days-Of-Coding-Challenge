0bc2eaa4452471640d6fd602ab71dce6ed90057c
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-21 16:30:55 +0500

    Question 144

--- Day48/main.ts
+++ Day48/main.ts
@@ -19,5 +19,13 @@ conditionalPromise
   .then((result) => console.log(result))
   .catch((error) => console.log(error.message));
 
-
 // Question 144:
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise<string>((resolve) => {
+  setTimeout(resolve, 100, "foo");
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
