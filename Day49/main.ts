21dbf6e42d0bb8fcb02e5308c3d03665774d8306
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-21 16:38:47 +0500

    Question 147

--- Day49/main.ts
+++ Day49/main.ts
@@ -19,3 +19,24 @@ const filteredNumbers: number[] = numbers.filter((number) => number > 5)
 console.log(filteredNumbers);
 
 // Question 147:
+function fetchData(
+    callback: (error: Error | null, data?: string) => void
+  ): void {
+    const error = new Error("Failed to fetch data");
+    const data = "Some data";
+  
+    if (Math.random() > 0.5) {
+      callback(null, data);
+    } else {
+      callback(error);
+    }
+  }
+  
+  fetchData((error, data) => {
+    if (error) {
+      console.error(error.message);
+    } else {
+      console.log(data);
+    }
+  });
+  
