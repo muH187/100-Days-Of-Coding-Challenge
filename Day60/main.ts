8596db853be058828ce113fd3b9822abaad11bb7
Muhammad Ali <proqualitythings100@gmail.com>
2024-04-29 15:03:32 +0500

    day 60

---
+++ Day60/main.ts
@@ -0,0 +1,16 @@
+// Function Declaration
+function add(a, b) {
+    return a + b
+}
+console.log(add(10, 50))
+
+let subtract = function(c, d) {
+    return c - d
+}
+console.log(subtract(30, 15))
+
+let myName = "Ali"
+function greet() {
+    return `Hello ${myName}! How are you?`
+}
+console.log(greet())
