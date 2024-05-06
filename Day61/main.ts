3e2d6890b23991212076bd9ee0367f65bd818435
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-06 16:53:05 +0500

    day 61

---
+++ Day61/main.ts
@@ -0,0 +1,13 @@
+// Arrow Function
+let sum = (a, b, c) => {
+    return a + b + c
+}
+console.log(sum(10,50,30))
+
+let avg = (num1, num2, num3, num4) => {
+    return num1 + num2 + num3 + num4 / 4
+}
+console.log(avg(20, 20, 20, 20))
+// Short Function:
+let double = n => n * 2
+console.log(double(10))
