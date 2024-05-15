0c963d31f4453d810ffff7e41371e912f92864e3
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-15 12:42:14 +0500

    day 63

---
+++ Day63/main.ts
@@ -0,0 +1,14 @@
+// Type Annotations:
+let myName: string = "Muhammad Ali"
+let myAge: number = 19
+let isBoy: boolean = true
+console.log(`His name is ${myName}, his age is ${myAge}, and is he boy? ${isBoy}`)
+
+// Dynamic Type:
+let friendName = "Tanzeel"
+console.log(friendName)
+
+// Any Type:
+let myVariable: any = 42
+myVariable = "Hello, TypeScript"
+console.log(myVariable)
