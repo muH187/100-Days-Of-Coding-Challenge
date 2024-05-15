503cf74cf5b17dc170331578fe3c8db46fdb716e
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-15 12:48:58 +0500

    day 64

---
+++ Day64/main.ts
@@ -0,0 +1,21 @@
+// Annotations With Objects:
+
+let personDetails: {
+    name: string
+    age: number
+    jobTitle: string
+    address: {
+        street: string
+        city: string
+    }
+}
+
+personDetails = {
+    name: "Muhammad Ali",
+    age: 19,
+    jobTitle: "Programmer",
+    address: {
+        street: "Federal B Area",
+        city: "Karachi"
+    }
+}
